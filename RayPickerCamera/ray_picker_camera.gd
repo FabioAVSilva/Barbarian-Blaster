8d7d7605a811a1a87aa10ce0334526eec1351885
Fabio Silva <fabio.silva99@hotmail.com>
2025-10-18 20:27:30 +0100

    Changed cell update function to work on mouse click
    -Mouse cursor now updates when hovering over valid cells

--- RayPickerCamera/ray_picker_camera.gd
+++ RayPickerCamera/ray_picker_camera.gd
@@ -10,12 +10,16 @@ func _process(delta: float) -> void:
 	var mouse_position: Vector2 = get_viewport().get_mouse_position()
 	ray_cast_3d.target_position = project_local_ray_normal(mouse_position) * 100.0
 	ray_cast_3d.force_raycast_update()
+	
 	if ray_cast_3d.is_colliding():
+		Input.set_default_cursor_shape(Input.CURSOR_POINTING_HAND)
 		var collider = ray_cast_3d.get_collider()
 		if collider is GridMap:
-			var collision_point =  ray_cast_3d.get_collision_point()
-			var cell = gridmap.local_to_map(collision_point)
-			if gridmap.get_cell_item(cell) == 0:
-				gridmap.set_cell_item(cell, 1)
-			
+			if Input.is_action_pressed("click"):
+				var collision_point =  ray_cast_3d.get_collision_point()
+				var cell = gridmap.local_to_map(collision_point)
+				if gridmap.get_cell_item(cell) == 0:
+					gridmap.set_cell_item(cell, 1)
+	else:
+		Input.set_default_cursor_shape()
 			
