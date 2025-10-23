ac1ecb719ca4fd44237d70feb110bbe62cb391c5
Fabio Silva <fabio.silva99@hotmail.com>
2025-10-23 21:14:56 +0100

    Implemented timers
    -Implemented timers to handle shot timing and to clean lingering projectiles after a set time
    -Set projectile spawn point to turret barrel

--- Turret/turret.gd
+++ Turret/turret.gd
@@ -1,7 +1,9 @@
 extends Node3D
 
 @export var projectile: PackedScene
+@onready var turret_barrel_ring: MeshInstance3D = $TurretBase/TurretTop/TurretBarrel3/TurretBarrelRing
 
-func _ready() -> void:
+func _on_timer_timeout() -> void:
 	var shot = projectile.instantiate()
 	add_child(shot)
+	shot.global_position = turret_barrel_ring.global_position
