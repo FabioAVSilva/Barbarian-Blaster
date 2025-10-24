59bae0c9f6baab369bb0ba95c630197e3e5f42fb
Fabio Silva <fabio.silva99@hotmail.com>
2025-10-25 00:10:07 +0100

    Implemented turret rotation
    -Turret manager takes the enemy path scene and passes it to any created turret
    -Created turrets take the last enemy's direction relative to the turret and use it to change their direction as well as the direction of the shot

--- Turret/turret.gd
+++ Turret/turret.gd
@@ -1,9 +1,17 @@
 extends Node3D
 
 @export var projectile: PackedScene
+
+var enemy_path: Path3D
+
 @onready var turret_barrel_ring: MeshInstance3D = $TurretBase/TurretTop/TurretBarrel3/TurretBarrelRing
+
+func _physics_process(delta: float) -> void:
+	var enemy = enemy_path.get_children().back()
+	look_at(enemy.global_position, Vector3.UP, true)
 
 func _on_timer_timeout() -> void:
 	var shot = projectile.instantiate()
 	add_child(shot)
 	shot.global_position = turret_barrel_ring.global_position
+	shot.direction = global_transform.basis.z
