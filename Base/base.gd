1a755e0788b5a064b3b4f571f94010c9583ef966
Fabio Silva <fabio.silva99@hotmail.com>
2025-11-24 18:08:56 +0000

    Added enemy health difficulty curve
    -Enemies will scale their health according to total time passed
    -After a set amount of time enemies stop spawning

--- Base/base.gd
+++ Base/base.gd
@@ -8,7 +8,6 @@ var white = Color.WHITE
 var current_health: int:
 	set(health_in):
 		current_health = health_in
-		print("Health was changed")
 		label_3d.text = str(current_health) + "/" + str(max_health)
 		label_3d.modulate = red.lerp(white, float(current_health) / float(max_health) )
 		if current_health < 1: 
@@ -20,6 +19,5 @@ func _ready() -> void:
 	current_health = max_health
 
 func take_damage() -> void:
-	print("Damage dealt to base!")
 	current_health -= 1
 	
