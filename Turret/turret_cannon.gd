d7083e0646b0d7ca737c2be1155e27680d719916
Fabio Silva <fabio.silva99@hotmail.com>
2025-12-11 22:24:51 +0000

    Switched meshes on Base and Turret
    -Switched the mesh on the Base scene for a high fidelity mesh
    -Created new Cannon Turret that replicates the functionality of the base turret, but with different animations and high fidelity assets

---
+++ Turret/turret_cannon.gd
@@ -0,0 +1,37 @@
+extends Node3D
+
+@export var projectile: PackedScene
+@export var turret_range := 10.0
+
+var enemy_path: Path3D
+var target: PathFollow3D
+
+@onready var turret_base: Node3D = $TurretBase
+@onready var turret_top: Node3D = $TurretBase/TurretTop
+@onready var cannon: Node3D = $TurretBase/TurretTop/Cannon
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+func _physics_process(delta: float) -> void:
+	target = find_best_target()
+	if target:
+		turret_top.look_at(target.global_position, Vector3.UP, true)
+
+func _on_timer_timeout() -> void:
+	if target:
+		var shot = projectile.instantiate()
+		add_child(shot)
+		shot.global_position = cannon.global_position
+		shot.direction = turret_top.global_transform.basis.z
+		animation_player.play("fire")
+	
+func find_best_target() -> PathFollow3D:
+	var best_target = null
+	var highest_progress = 0
+	for enemy in enemy_path.get_children():
+		if enemy is PathFollow3D:
+			var distance = global_position.distance_to(enemy.global_position)
+			if enemy.progress > highest_progress && distance <= turret_range:
+				best_target = enemy
+				highest_progress = enemy.progress
+				
+	return best_target
